5d250bf4ed5c30a547344a59365cb145ef0bd3f3
Jenei Marcell <55838469+Makkareal@users.noreply.github.com>
2026-04-22 17:41:37 +0200

    Update multilog.sh

--- multilog.sh
+++ multilog.sh
@@ -1,32 +1,31 @@
 #!/bin/bash
 
 # --- CONFIGURATION ---
-STATS_FILE="${1:-build_stats.csv}"
-LOG_FILE="${2:-compilation.log}"
-count="${3:-10}"
-MEMORY="${4:-850MB}"
-MEMORY_SWAP="${5:-3G}"
+Folder="${1:-logFolder}"
+count="${2:-10}"
+MEMORY="${3:-850MB}"
+MEMORY_SWAP="${4:-3G}"
 # ---------------------
+mkdir $Folder
+
 for i in $(seq $count); do
+echo "./Folder/$i-log";
   DOCKER_ID=$(
   docker run -i -d\
     --memory="$MEMORY"\
     --memory-swap="$MEMORY_SWAP"\
     -v /path/to/llvm:/path/to/llvm \
     -w path/to/build/folder \
-    DOCKER-IMAGE
+    llvm-arch-builder
     )
   DOCKER_NAME=$(docker inspect --format="{{.Name}}" $DOCKER_ID)
     docker exec $DOCKER_NAME rm ./bin/binary-to-remove
     echo "Removed binary"
-    ./sublog.sh $DOCKER_NAME "$STATS_FILE" &
+    ./sublog.sh $DOCKER_NAME "./$Folder/$i-stats" &
     echo "Logger started"
     docker exec $DOCKER_NAME ninja what-to-build >> "$LOG_FILE"
     echo "linking finished"
     docker stop $DOCKER_NAME >> /dev/null
     docker rm $DOCKER_NAME >> /dev/null
-    echo "" >>$STATS_FILE
-    echo "" >>$STATS_FILE
-    echo "" >>$STATS_FILE
     echo "docker removed"
 done
